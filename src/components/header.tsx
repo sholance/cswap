f1bafabd2e019feec8ebb23eb0c1e589eb1ff6b9
xhola <ohksamie@gmail.com>
2022-12-20 07:18:07 +0100

    responsive nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -18,6 +18,8 @@ export default function Navbar() {
 
     const [price, setPrice] = useState(0.500);
     const [loading, setLoading] = useState(true);
+    const [toggleMenu, setToggleMenu] = useState(false)
+
 
     const { connect, address } = useContractKit();
     const fetchPrice = () => {
@@ -67,9 +69,9 @@ export default function Navbar() {
             }${address.slice(address.length - 4)}`
             : address;
     }
-    const navClick = (e: { currentTarget: { classList: { toggle: (arg0: string) => void; }; }; }) => {
-        e.currentTarget.classList.toggle('responsive');
-    };
+    const toggleNav = () => {
+        setToggleMenu(!toggleMenu)
+    }
 
     return (
         <>
@@ -80,6 +82,36 @@ export default function Navbar() {
                             <Link to="/">CSWAP
                             </Link>
                         </div>
+                        {(toggleMenu && (
+                            <div className='mobile-nav'>
+                                <ul className='list'>
+                                    <div className='nav-items'>
+                                        <Link to="/trade">Trade</Link>
+                                    </div>
+                                    <div className='nav-items'>
+                                        <Link to="/swap">Swap</Link>
+                                    </div>
+                                    <div> {loading ? (
+                                        <div className='nav-item price loading-price'>
+                                            <>
+                                                <span className='nav-price-text'>
+                                                    celo today
+                                                </span>
+                                                $
+                                            </>
+                                        </div>) : (
+                                        <div className='price'>
+                                            <>
+                                                <span className='nav-price-text'>
+                                                    celo today
+                                                </span>
+                                                ${price}
+                                            </>
+                                        </div>)
+                                    }</div>
+                                </ul>
+                            </div>
+                        ))}
                         <div className='nav-link'>
                             <div className='nav-item'>
                                 <Link to="/trade">Trade</Link>
@@ -115,7 +147,7 @@ export default function Navbar() {
                             )
                             }
                         </div>
-                        <a id="menu-icon" onClick={navClick} className="menu-icon">
+                        <a id="menu-icon" className="menu-icon responsive" onClick={toggleNav}>
                             <svg width="35" height="35" viewBox="0 0 24 24" fill="none" stroke="#3d2b7c" stroke-width="2" stroke-linecap="butt" stroke-linejoin="arcs"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg>
                         </a>
                     </nav>
